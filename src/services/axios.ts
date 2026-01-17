e908301fd97a5e222af270ca34829457cc58aa8c
Anthony Sung <sungpinyue@gmail.com>
2026-01-17 22:48:37 +0800

    Prepare frontend for deployment

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
